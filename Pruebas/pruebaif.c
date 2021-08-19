259e74d83ed703f6969bf911c00b7898f2d40fe0
sebastian r <sebastian.ramirezar@upb.edu.co>
2021-08-19 17:47:34 -0500

    Primer Comid para el curso de sistemas operativos (ejercicios 1 a 4)

---
+++ Pruebas/pruebaif.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main(int argc,char*argv[]){
+    int n=0;
+    if (n==1)
+    {
+        printf("Salio por el 1");
+    }
+    else
+    {
+        printf("salio por otro lado");
+    }
+}
